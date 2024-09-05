71d08fd2939235713c2c7660a070226afc3be66c
Sulav Karki <sulavkarki08@gmail.com>
2024-09-05 11:53:17 +0545

    sec

---
+++ cashcarefrontend/lib/utils/cards/transaction_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class TransactionCard extends StatelessWidget {
+  final String title;
+  final TransactionCardData td;
+  final String amount;
+  final String date;
+  final String type;
+
+  TransactionCard(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.td,
+      required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10), // Rounded corners
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      color: Colors.grey[900], // Dark background color
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Rs $amount',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color:
+                        td.identifier == 'income' ? Colors.green : Colors.red,
+                  ),
+                ),
+                Text(
+                  date,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ cashcarefrontend/lib/utils/cards/upcoming_payment_card.dart
@@ -0,0 +1,66 @@
+import 'package:cashcarefrontend/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class UpcomingPaymentCard extends StatelessWidget {
+  final Payment payment;
+  UpcomingPaymentCard({required this.payment});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: AppColors.lowDarkBlue,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                const SizedBox(width: 10),
+                Text(
+                  payment.name,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                const Icon(Icons.access_time, color: Colors.orangeAccent),
+                const SizedBox(width: 5),
+                Text(
+                  "Due in ${payment.due} day",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(children: [
+              const Icon(color: Colors.green, Icons.money),
+              const SizedBox(width: 10),
+              Text(
+                "${payment.savings} / ${payment.amount} ",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              )
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
