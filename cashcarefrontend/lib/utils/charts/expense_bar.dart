e70a4001a14df294aa96a0ec70a094412f985cad
Sulav Karki <sulavkarki08@gmail.com>
2024-09-08 11:11:18 +0545

    main

---
+++ cashcarefrontend/lib/utils/charts/expense_bar.dart
@@ -0,0 +1,121 @@
+import 'package:cashcarefrontend/constants/app_colors.dart';
+import 'package:cashcarefrontend/models/category.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class ExpenseBarChart extends StatelessWidget {
+  final RxList<Category> categories;
+  final Color color;
+
+  ExpenseBarChart({required this.categories, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+        aspectRatio: 1.2,
+        child: Card(
+            elevation: 4,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            color: AppColors.black,
+            child: Padding(
+              padding: const EdgeInsets.all(1),
+              child: Obx(
+                () => BarChart(
+                  BarChartData(
+                    alignment: BarChartAlignment.spaceAround,
+                    maxY: categories
+                            .map((e) => e.amount)
+                            .reduce((a, b) => a > b ? a : b) +
+                        10.0,
+                    barTouchData: BarTouchData(
+                      touchTooltipData: BarTouchTooltipData(
+                        tooltipBgColor: Colors.blueGrey,
+                        getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                          return BarTooltipItem(
+                            '${rod.toY}',
+                            TextStyle(color: Colors.yellow),
+                          );
+                        },
+                      ),
+                    ),
+                    titlesData: FlTitlesData(
+                        show: true,
+                        topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        rightTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (double value, title) {
+                              return Text(categories[value.toInt()].category);
+                            },
+                            //getTitlesWidget: (double value, title) {
+                            //  switch (value.toInt()) {
+                            //    case 0:
+                            //      return Text('Sun');
+                            //    case 1:
+                            //      return Text('Mon');
+                            //    case 2:
+                            //      return Text('Tue');
+                            //    case 3:
+                            //      return Text('Wes');
+                            //    case 4:
+                            //      return Text('Thu');
+                            //    case 5:
+                            //      return Text('Fir');
+                            //    case 6:
+                            //      return Text('Sat');
+                            //    default:
+                            //      return Text('');
+                            //  }
+                            //},
+                          ),
+                        )),
+                    barGroups: categories.asMap().entries.map((entry) {
+                      final index = entry.key;
+                      final category = entry.value;
+                      return BarChartGroupData(
+                        x: index,
+                        barRods: [
+                          BarChartRodData(
+                            toY: category.amount.toDouble(),
+                            color: getColor(index),
+                            width: 30,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            )));
+  }
+
+  Color getColor(int index) {
+    if (index == -1) {
+      return Colors.red;
+    }
+    return AppColors.pieChartColors[index];
+  }
+
+  Color getCategoryColor(String category) {
+    switch (category) {
+      case 'Food':
+        return Colors.green;
+      case 'Transport':
+        return Colors.green;
+      case 'Entertainment':
+        return Colors.green;
+      case 'Utilities':
+        return Colors.green;
+      case 'Others':
+        return Colors.green;
+      default:
+        return Colors.green;
+    }
+  }
+}
---
+++ cashcarefrontend/lib/utils/charts/pie_chart.dart
@@ -0,0 +1,161 @@
+import 'package:cashcarefrontend/constants/app_colors.dart';
+import 'package:cashcarefrontend/data/graph_data.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:get/get.dart';
+
+class MyPieChart extends StatelessWidget {
+  final RxList<PieData> data;
+  MyPieChart(this.data, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Obx(
+                  () {
+                    double otherValue = 0.0;
+                    List<PieChartSectionData> sections = [];
+                    for (int i = 0; i < data.length; i++) {
+                      if (i < 3) {
+                        sections.add(PieChartSectionData(
+                          showTitle: false,
+                          titleStyle: const TextStyle(color: Colors.white),
+                          value: data[i].value,
+                          title: data[i].name,
+                          color: getColor(i),
+                          radius: 20,
+                        ));
+                      } else {
+                        otherValue += data[i].value;
+                      }
+                    }
+
+                    if (otherValue > 0) {
+                      sections.add(PieChartSectionData(
+                        showTitle: false,
+                        titleStyle: const TextStyle(color: Colors.white),
+                        value: otherValue,
+                        title: 'Other',
+                        color: getColor(3),
+                        radius: 20,
+                      ));
+                    }
+
+                    return data.isNotEmpty
+                        ? PieChart(
+                            PieChartData(
+                              sectionsSpace: 4,
+                              borderData: FlBorderData(show: false),
+                              centerSpaceRadius: 50,
+                              sections: sections,
+                            ),
+                            swapAnimationDuration:
+                                const Duration(milliseconds: 150), // Optional
+                            swapAnimationCurve: Curves.linear,
+                          )
+                        : const Text("Data not found");
+                  },
+                ),
+                Obx(() => Text(
+                      data
+                          .fold(0.0, (sum, item) => sum + item.value)
+                          .toString(),
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          Obx(() {
+            double otherValue = 0.0;
+            List<PieIndicator> indicators = [];
+            for (int i = 0; i < 3 && i < data.length; i++) {
+              indicators.add(PieIndicator(
+                color: getColor(i),
+                isSquare: false,
+                text: data[i].name,
+                textColor: AppColors.white,
+              ));
+            }
+            for (int i = 3; i < data.length; i++) {
+              otherValue += data[i].value;
+            }
+            if (otherValue > 0) {
+              indicators.add(PieIndicator(
+                color: getColor(3),
+                isSquare: false,
+                text: 'Others',
+                textColor: AppColors.white,
+              ));
+            }
+            return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: indicators);
+          }),
+        ],
+      ),
+    );
+  }
+
+  Color getColor(int index) {
+    return AppColors.pieChartColors[index % AppColors.pieChartColors.length];
+  }
+}
+
+class PieIndicator extends StatelessWidget {
+  const PieIndicator({
+    super.key,
+    required this.color,
+    required this.text,
+    required this.isSquare,
+    this.size = 16,
+    this.textColor,
+  });
+
+  final Color color;
+  final String text;
+  final bool isSquare;
+  final double size;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: textColor ?? Colors.black,
+            overflow: TextOverflow.fade,
+          ),
+        ),
+      ],
+    );
+  }
+}
