846b701645fdfd3fa329ad953ecaccf4debb2274
Sulav Karki <sulavkarki08@gmail.com>
2024-09-10 11:22:17 +0545

    main

---
+++ cashcarefrontend/lib/utils/textfields/ww_textfield.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CCTextfield extends StatelessWidget {
+  const CCTextfield({
+    required this.controller,
+    required this.icon,
+    required this.validator,
+    required this.label,
+    super.key,
+  });
+  final TextEditingController controller;
+  final Widget icon;
+  final String label;
+  final String? Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      decoration: InputDecoration(
+        hoverColor: Colors.white,
+        focusColor: Colors.white,
+        fillColor: Colors.white,
+        prefixIcon: icon,
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.white),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20), // Make the border rounded
+          borderSide: const BorderSide(color: Colors.white),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20), // Make the border rounded
+          borderSide: const BorderSide(color: Colors.white),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20), // Make the border rounded
+          borderSide: const BorderSide(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
