a89261bf80a6f8507c644c861c78313969592f3a
Sulav Karki <sulavkarki08@gmail.com>
2024-10-04 22:16:30 +0545

    main

--- cashcarefrontend/lib/constants/app_constant.dart
+++ cashcarefrontend/lib/constants/app_constant.dart
@@ -1,19 +1,8 @@
 class AppConstant {
   AppConstant._();
-  static const baseUrl = "http://192.168.1.91:8000";
-  // static const baseUrl = "http://192.168.100.34:8000";
+  // static const baseUrl = "http://192.168.1.91:8000";
+  static const baseUrl = "http://192.168.100.34:8000";
 }
-
-// String getFirstandLastNameInitals(String fullName) {
-//   // if (fullName.isEmpty) {
-//   //   return 'N/A';
-//   // }
-//   if (fullName.split(' ').length == 1) {
-//     return fullName[0];
-//   }
-//   List<String> name = fullName.split(' ');
-//   return name[0][0] + name[1][0];
-// }
 
 String getFirstandLastNameInitals(String fullName) {
   if (fullName.isEmpty) {
